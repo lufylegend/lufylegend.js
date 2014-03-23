6cd1f2b130df749f8f2734b05420d213ae0021e1
Yorhom <wangyuehao1999@gmail.com>
2014-03-23 22:46:38 +0800

    Update LMouseEventContainer.js

--- src/events/LMouseEventContainer.js
+++ src/events/LMouseEventContainer.js
@@ -1,16 +1,4 @@
 /*
- * LMouseEvent.js
- **/
-var LMouseEvent = function (){throw "LMouseEvent cannot be instantiated";};
-LMouseEvent.MOUSE_DOWN = "mousedown";
-LMouseEvent.MOUSE_UP = "mouseup";
-LMouseEvent.TOUCH_START = "touchstart";
-LMouseEvent.TOUCH_MOVE = "touchmove";
-LMouseEvent.TOUCH_END = "touchend";
-LMouseEvent.MOUSE_MOVE = "mousemove";
-LMouseEvent.MOUSE_OUT = "mouseout";
-LMouseEvent.DOUBLE_CLICK = "dblclick";
-/*
  * LMouseEventContainer.js
  **/
 function $LMouseEventContainer(){
