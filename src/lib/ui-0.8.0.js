bcf5c2f36f19b708c0931834386df0ec3bb64f73
lufy <lufy.legend@gmail.com>
2014-05-11 01:38:57 +0900

    hitTest LMenubar

--- src/lib/ui-0.8.0.js
+++ src/lib/ui-0.8.0.js
@@ -995,6 +995,7 @@ function LMenubar(list,style){
 	if(!style.textColor)style.textColor="#000000";
 	if(!style.lineColor)style.lineColor="#CCCCCC";
 	if(!style.backgroundColor)style.backgroundColor="#FFFFFF";
+	if(!style.itemBackgroundColor)style.itemBackgroundColor=style.backgroundColor;
 	if(!style.selectColor)style.selectColor="#1E90FF";
 	s.style = style;
 	var back = new LSprite();
@@ -1022,11 +1023,115 @@ function LMenubar(list,style){
 		root.open = false;
 		setTimeout(function(){
 			root.back.visible = false;
+			root.dispatchEvent(LMenubar.MENU_CLOSE);
 		},100);
 	});
 	s.back.visible = false;
 	s.setList(s,list,0,0,0);
 }
+LMenubar.MENU_CLOSE = "menu_close";
+LMenubar.prototype.openMainMenu = function(index){
+	var self = this;
+	self.mousedown({clickTarget:self.getChildAt(index+1)});
+};
+LMenubar.prototype.mousedown = function(e){
+	var target = e.clickTarget;
+	var root = target.root;
+	if(target.mainMenu){
+		if(root.open)return;
+		root.open = true;
+		root.back.visible = true;
+		root.select = target;
+		var sW = target.getWidth();
+		var sH = target.getHeight();
+		target.graphics.clear();
+		target.graphics.drawRect(0,root.style.selectColor,[0,0,sW,sH],true,root.style.selectColor);
+
+		if(target.menuList && target.menuList.length){
+			for(var j=0;j<target.menuList.length;j++){
+				target.menuList[j].visible = true;
+				target.menuList[j].graphics.clear();
+				target.menuList[j].graphics.drawRect(1,root.style.lineColor,[0,0,target.childWidth,target.childHeight],true,root.style.itemBackgroundColor);
+				if(target.menuList[j].arrow){
+					target.menuList[j].arrow.x = target.childWidth - root.style.spaceHorizontal*2;
+				}
+			}
+		}
+		return;
+	}
+	if(!target.menuList){
+		if(target.click){
+			target.click({target:root});
+			root.open = false;
+			setTimeout(function(){
+				root.back.visible = false;
+			},100);
+		}
+		for(var j=0;j<root.childList.length;j++){
+			if(root.childList[j].mainMenu)continue;
+			root.childList[j].visible = false;
+		}
+	}
+};
+LMenubar.prototype.mousemove = function(e){
+	var target = e.clickTarget;
+	var root = target.root;
+	if(!root.open)return;
+	if(root.select && root.select.objectIndex == target.objectIndex){
+		return;
+	}
+	if(root.select){
+		var rW = root.select.getWidth();
+		var rH = root.select.getHeight();
+		root.select.graphics.clear();
+		root.select.graphics.drawRect(root.select.mainMenu ? 0 : 1,root.style.lineColor,[0,0,rW,rH],true,root.select.mainMenu?root.style.backgroundColor:root.style.itemBackgroundColor);
+	}
+	var sW = target.getWidth();
+	var sH = target.getHeight();
+	target.graphics.clear();
+	target.graphics.drawRect(target.mainMenu ? 0 : 1,root.style.lineColor,[0,0,sW,sH],true,root.style.backgroundColor);
+	target.graphics.drawRect(0,root.style.selectColor,[0,root.style.spaceVertical,sW,sH-root.style.spaceVertical*2],true,root.style.selectColor);
+	if(target.mainMenu){
+		for(var j=0;j<root.childList.length;j++){
+			if(root.childList[j].mainMenu)continue;
+			root.childList[j].visible = false;
+		}
+	}else if(root.select.depth == target.depth){
+		if(root.select.menuList && root.select.menuList.length){
+			for(var j=0;j<root.select.menuList.length;j++){
+				root.select.menuList[j].visible = false;
+			}
+		}
+	}else if(root.select.depth > target.depth){
+		if(root.select.upper.menuList && root.select.upper.menuList.length){
+			for(var j=0;j<root.select.upper.menuList.length;j++){
+				root.select.upper.menuList[j].visible = false;
+			}
+		}
+	}
+	if(target.menuList && target.menuList.length){
+		for(var j=0;j<target.menuList.length;j++){
+			target.menuList[j].visible = true;
+			target.menuList[j].graphics.clear();
+			target.menuList[j].graphics.drawRect(1,root.style.lineColor,[0,0,target.childWidth,target.childHeight],true,target.menuList[j].mainMenu?root.style.backgroundColor:root.style.itemBackgroundColor);
+			if(!target.mainMenu){
+				target.menuList[j].x = target.x + target.getWidth();
+			}
+			if(target.menuList[j].arrow){
+				target.menuList[j].arrow.x = target.childWidth - root.style.spaceHorizontal*2;
+			}
+		}
+	}
+	root.select = target;
+};
+LMenubar.prototype.mainMenuHide = function(value){
+	var self = this;
+	for(var j=0;j<self.childList.length;j++){
+		if(self.childList[j].mainMenu){
+			self.childList[j].visible = false;
+		}
+	}
+};
 LMenubar.prototype.setList = function(layer,list,depth,sx,sy){
 	var s = this,w=0,h=0,menuList=[];
 	layer.childWidth = 0;
@@ -1047,101 +1152,8 @@ LMenubar.prototype.setList = function(layer,list,depth,sx,sy){
 		label.y = s.style.spaceVertical;
 		menu.addChild(label);
 		menu.graphics.drawRect(0,s.style.backgroundColor,[0,0,label.getWidth()+s.style.textSize,label.getHeight()+s.style.textSize],true,s.style.backgroundColor);
-		menu.addEventListener(LMouseEvent.MOUSE_DOWN,function(e){
-			var target = e.clickTarget;
-			var root = target.root;
-			if(target.mainMenu){
-				if(root.open)return;
-				root.open = true;
-				root.back.visible = true;
-				root.select = target;
-				var sW = target.getWidth();
-				var sH = target.getHeight();
-				target.graphics.clear();
-				target.graphics.drawRect(0,root.style.selectColor,[0,0,sW,sH],true,root.style.selectColor);
-
-				if(target.menuList && target.menuList.length){
-					for(var j=0;j<target.menuList.length;j++){
-						target.menuList[j].visible = true;
-						target.menuList[j].graphics.clear();
-						target.menuList[j].graphics.drawRect(1,root.style.lineColor,[0,0,target.childWidth,target.childHeight],true,root.style.backgroundColor);
-						if(target.menuList[j].arrow){
-							target.menuList[j].arrow.x = target.childWidth - s.style.spaceHorizontal*2;
-						}
-					}
-				}
-				return;
-			}
-			if(!target.menuList){
-				if(target.click){
-					target.click({target:s});
-					root.open = false;
-					setTimeout(function(){
-						root.back.visible = false;
-					},100);
-				}
-				for(var j=0;j<root.childList.length;j++){
-					if(root.childList[j].mainMenu)continue;
-					root.childList[j].visible = false;
-				}
-			}
-		});
-		menu.addEventListener(LMouseEvent.MOUSE_MOVE,function(e){
-			var target = e.clickTarget;
-			var root = target.root;
-			if(!root.open)return;
-			if(root.select && root.select.objectIndex == target.objectIndex){
-				return;
-			}
-			if(root.select){
-				var rW = root.select.getWidth();
-				var rH = root.select.getHeight();
-				root.select.graphics.clear();
-				root.select.graphics.drawRect(root.select.mainMenu ? 0 : 1,root.style.lineColor,[0,0,rW,rH],true,root.style.backgroundColor);
-
-			}
-			var sW = target.getWidth();
-			var sH = target.getHeight();
-			target.graphics.clear();
-			target.graphics.drawRect(target.mainMenu ? 0 : 1,root.style.lineColor,[0,0,sW,sH],true,root.style.backgroundColor);
-			target.graphics.drawRect(0,root.style.selectColor,[0,root.style.spaceVertical,sW,sH-root.style.spaceVertical*2],true,root.style.selectColor);
-
-			if(target.mainMenu){
-				for(var j=0;j<root.childList.length;j++){
-					if(root.childList[j].mainMenu)continue;
-					root.childList[j].visible = false;
-				}
-			}else if(root.select.depth == target.depth){
-				if(root.select.menuList && root.select.menuList.length){
-					for(var j=0;j<root.select.menuList.length;j++){
-						root.select.menuList[j].visible = false;
-					}
-				}
-			}else if(root.select.depth > target.depth){
-				if(root.select.upper.menuList && root.select.upper.menuList.length){
-					for(var j=0;j<root.select.upper.menuList.length;j++){
-						root.select.upper.menuList[j].visible = false;
-					}
-				}
-			}
-
-			if(target.menuList && target.menuList.length){
-				for(var j=0;j<target.menuList.length;j++){
-					target.menuList[j].visible = true;
-					target.menuList[j].graphics.clear();
-					target.menuList[j].graphics.drawRect(1,root.style.lineColor,[0,0,target.childWidth,target.childHeight],true,root.style.backgroundColor);
-
-					if(!target.mainMenu){
-						target.menuList[j].x = target.x + target.getWidth();
-					}
-					if(target.menuList[j].arrow){
-						target.menuList[j].arrow.x = target.childWidth - s.style.spaceHorizontal*2;
-					}
-				}
-			}
-
-			root.select = target;
-		});
+		menu.addEventListener(LMouseEvent.MOUSE_DOWN,s.mousedown);
+		menu.addEventListener(LMouseEvent.MOUSE_MOVE,s.mousemove);
 		if(s.objectIndex == layer.objectIndex){
 			menu.x = w + sx;
 			menu.y = 0 + sy;
