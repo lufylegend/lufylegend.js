ede05f144696c9db8204aded63ce1b94fea9cb05
lubin.zhang <lubin.zhang@enish.jp>
2014-07-11 19:05:45 +0900

    update

---
+++ src/api/OldApi.js
@@ -0,0 +1,21 @@
+/** @language chinese
+ * <p>因为新版API还在继续更新，还未更新的内容，请先查看旧版API，旧版api地址如下：</p>
+ * <p><a href="http://lufylegend.com/lufylegend/oldapi" target="_blank">http://lufylegend.com/lufylegend/oldapi</a></p>
+ * @class 旧版API
+ * @since 1.9.0
+ * @public
+ */
+/** @language english
+ * <p>Because the Api of new version is being updated，the API of the old version：</p>
+ * <p><a href="http://lufylegend.com/lufylegend/oldapi" target="_blank">http://lufylegend.com/lufylegend/oldapi</a></p>
+ * @class old API
+ * @since 1.9.0
+ * @public
+ */
+/** @language japanese
+ * <p>新しいバージョンのAPIは更新中ですから、まだ更新していない部分ですが、古いバージョンのAPIを参考してください，古いバージョンのapiのURLは下記となります：</p>
+ * <p><a href="http://lufylegend.com/lufylegend/oldapi" target="_blank">http://lufylegend.com/lufylegend/oldapi</a></p>
+ * @class 古いバージョンのAPI
+ * @since 1.9.0
+ * @public
+ */
