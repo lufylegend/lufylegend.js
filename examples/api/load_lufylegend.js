0c7b98db17cacc31e2a15bcc86fbfc51b7389398
lufy <lufy.legend@gmail.com>
2014-11-10 02:24:15 +0900

    api update

--- examples/api/load_lufylegend.js
+++ examples/api/load_lufylegend.js
@@ -1,1 +1,1 @@
-document.write('<script type="text/javascript" src="../../../lufylegend-1.9.4.min.js"></script> ');
+document.write('<script type="text/javascript" src="../../../lufylegend-1.9.5.min.js"></script> ');
