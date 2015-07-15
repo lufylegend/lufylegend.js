f09e7cf5e6a4dcb89f50e5a21692a304c19f9531
lubin.zhang <lubin.zhang@enish.jp>
2015-07-15 11:07:07 +0900

    1.9.10 build

--- examples/demo/load_lufylegend.js
+++ examples/demo/load_lufylegend.js
@@ -1,1 +1,1 @@
-document.write('<script type="text/javascript" src="../../../lufylegend-1.9.9.min.js"></script> ');
+document.write('<script type="text/javascript" src="../../../lufylegend-1.9.10.min.js"></script> ');
