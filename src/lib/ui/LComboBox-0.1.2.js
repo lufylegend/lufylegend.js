e8136a255e736ae6f629450169c108b4ca34af5a
lufy <lufy.legend@gmail.com>
2016-01-03 17:02:08 +0900

    LComboBox被删除后，内部事件不清除的bug

--- src/lib/ui/LComboBox-0.1.2.js
+++ src/lib/ui/LComboBox-0.1.2.js
@@ -220,6 +220,12 @@ var LComboBox = (function () {
 			s.setValue(child.value);
 		}
 	};
+	LComboBox.prototype.die = function () {
+		var self = this;
+		self.listView.die();
+		self.listView.removeAllChild();
+		self.callParent("die",arguments);
+	};
 	LComboBox.prototype._showChildList = function (event) {
 		var s = event.currentTarget;
 		s.showChildList();
@@ -364,6 +370,11 @@ var LComboBoxChild = (function () {
 		var listViewIndex;
 		comboBox.setValue(self.content.value);
 		for(var i=LGlobal.stage.numChildren-1;i>=0;i--){
+			if(LGlobal.stage.childList[i].constructor.name == "LListViewDragObject"){
+				LGlobal.stage.removeChildAt(i);
+				LGlobal.listViewDragObject = null;
+				continue;
+			}
 			if(LGlobal.stage.childList[i].objectIndex != listView.objectIndex){
 				continue;
 			}
