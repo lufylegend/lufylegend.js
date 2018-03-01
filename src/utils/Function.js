48a57acd4550bbf16d1dd678a33b9bc8787a888c
lufy <lufy.legend@gmail.com>
2018-03-02 01:54:20 +0900

    add LAtlas

--- src/utils/Function.js
+++ src/utils/Function.js
@@ -169,6 +169,28 @@ if (!Array.prototype.some) {
 		return false;
 	};
 }
+if (!Array.prototype.filter) {
+	Array.prototype.filter = function(fun /*, thisp */) {
+		"use strict";
+		if (this == null) {
+			throw new TypeError();
+		}
+		var t = Object(this), len = t.length >>> 0;
+		if (typeof fun != "function") {
+			throw new TypeError();
+		}
+		var res = [], thisp = arguments[1];
+		for (var i = 0; i < len; i++) {
+			if (i in t) {
+				var val = t[i];
+				if (fun.call(thisp, val, i, t)) {
+					res.push(val);
+				}
+			}
+		}
+		return res;
+	};
+}
 if (!String.format) {
 	String.format = function(format) {
 	    var args = Array.prototype.slice.call(arguments, 1);
