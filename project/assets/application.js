6b9e1523b2b7faac36ace10534da05e76481c276
lufy <lufy.legend@gmail.com>
2018-03-14 01:26:47 +0900

    es6 update

--- project/assets/application.js
+++ project/assets/application.js
@@ -1,9 +1,9 @@
 
-import BaseManager from './plugin/mvc/managers/BaseManager';
-import LGlobal from './plugin/lufylegend/utils/LGlobal';
+//import BaseManager from './plugin/mvc/managers/BaseManager';
+//import LGlobal from './plugin/lufylegend/utils/LGlobal';
 import { LInit } from './plugin/lufylegend/utils/Function';
-import LAtlas from './plugin/lufylegend/system/LAtlas';
-import LEvent from './plugin/lufylegend/events/LEvent';
+//import LAtlas from './plugin/lufylegend/system/LAtlas';
+//import LEvent from './plugin/lufylegend/events/LEvent';
 
 
 
@@ -29,12 +29,12 @@ function loadBitmapdata(event) {
     let bitmap2 = new LBitmap(bitmapData2);
     bitmap2.y = 250;
     addChild(bitmap2);*/
-    LGlobal.debug = true;
-    let atlas = new LAtlas();
+    //LGlobal.debug = true;
+    /*let atlas = new LAtlas();
     atlas.addEventListener(LEvent.COMPLETE, () => {
         BaseManager.showDialog('Logo');
     });
-    atlas.load('resources/atlas', 'Card');
+    atlas.load('resources/atlas', 'Card');*/
 }
 /*
 function main() {
@@ -44,4 +44,4 @@ function main() {
         BaseManager.showDialog('Logo');
     });
     atlas.load('resources/atlas', 'Card');
-}*/
+}*/import LLoader from "./plugin/lufylegend/display/LLoader";import LBitmapData from "./plugin/lufylegend/display/LBitmapData";import LBitmap from "./plugin/lufylegend/display/LBitmap";import LSprite from "./plugin/lufylegend/display/LSprite";import LMouseEvent from "./plugin/lufylegend/events/LMouseEvent";import LRectangle from "./plugin/lufylegend/geom/LRectangle";import LPoint from "./plugin/lufylegend/geom/LPoint";import LTweenLite from "./plugin/lufylegend/transitions/LTweenLite";import LEasing from "./plugin/lufylegend/transitions/LEasing";import LogoController from "./scripts/controllers/LogoController";import BindSpriteView from "./plugin/mvc/views/BindSpriteView";import BindTextView from "./plugin/mvc/views/BindTextView";import BindListView from "./plugin/mvc/views/BindListView";import BindListChildView from "./plugin/mvc/views/BindListChildView";
