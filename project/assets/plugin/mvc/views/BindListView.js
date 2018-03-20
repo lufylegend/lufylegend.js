5aba4d2c881d834b7ecdd1445f59e376b99071e8
lufy <lufy.legend@gmail.com>
2018-03-20 18:55:44 +0900

    update

--- project/assets/plugin/mvc/views/BindListView.js
+++ project/assets/plugin/mvc/views/BindListView.js
@@ -6,7 +6,7 @@ import PrefabContainer from '../prefabs/PrefabContainer';
 class BindListView extends BaseBindView {
     init() {
         super.init();
-        this.listView = new LListView();
+        this._listView = new LListView();
         this.addChild(this.listView);
     }
     updateView() {
@@ -15,13 +15,20 @@ class BindListView extends BaseBindView {
         if (value === null) {
             return;
         }
-        let listView = this.listView;
+        let listView = this._listView;
+        for (let key in this.listView) {
+            if (key === 'size') {
+                listView.resize(300, 200);
+                return;
+            }
+            listView[key] + this.listView[key];
+        }
         listView.scrollBarHorizontal.showCondition === LListView.ScrollBarCondition.Always;
         listView.scrollBarVertical.showCondition === LListView.ScrollBarCondition.Always;
-        listView.maxPerLine = 1;
-        listView.cellWidth = 100;
-        listView.cellHeight = 100;
-        listView.resize(300, 200);
+        //listView.maxPerLine = 1;
+        //listView.cellWidth = 100;
+        //listView.cellHeight = 100;
+        //listView.resize(300, 200);
         listView.arrangement = LListView.Direction.Horizontal;
         listView.movement = LListView.Direction.Vertical;
 
@@ -38,7 +45,7 @@ class BindListView extends BaseBindView {
         this.model = model;
         let node = LNode.create(data);
         listChild.addChild(node);
-        this.listView.insertChildView(listChild);
+        this._listView.insertChildView(listChild);
         node.updateWidget(model);
     }
 }
