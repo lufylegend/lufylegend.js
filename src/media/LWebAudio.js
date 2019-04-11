b27698ebce23cf74018e7ab64c1f550713108346
lufy <lufy@lufynoiMac.local>
2019-04-11 22:42:00 +0900

    fix webaudio bug

--- src/media/LWebAudio.js
+++ src/media/LWebAudio.js
@@ -99,7 +99,7 @@ var LWebAudio = (function () {
 	var p = {
 		getWebAudio : function () {
 			var data;
-			if(LWebAudio.containerCount > 0){
+			if(LWebAudio.container.length > 0){
 				data = LWebAudio.container.shift();
 			} else {
 				if (typeof AudioContext !== UNDEFINED) {
@@ -354,6 +354,9 @@ var LWebAudio = (function () {
 				s.currentTimeTo = to > s.length ? s.length : to;
 			} else {
 				s.currentTimeTo = s.length;
+			}
+			if(s.data.resume){
+				s.data.resume();
 			}
 			s.data.loop = false;
 			s.playing = true;
